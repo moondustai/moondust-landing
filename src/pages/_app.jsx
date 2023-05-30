211e14aeaca2681099eeb65b479dcb23268f788a
Jordan Humphreys <mrsweaters@users.noreply.github.com>
2023-05-29 22:20:37 -0400

    Clean up metadata.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -12,10 +12,10 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Commit - Open-source Git client for macOS minimalists</title>
+        <title>Moondust.ai | Ads for ChatGPT</title>
         <meta
           name="description"
-          content="Commit is a lightweight Git client you can open from anywhere any time you’re ready to commit your work with a single keyboard shortcut. It’s fast, beautiful, and completely unnecessary."
+          content="Moondust.ai is an advertising platform for streaming ai chat applications built around ChatGPT."
         />
       </Head>
       <ThemeProvider attribute="class" disableTransitionOnChange>
