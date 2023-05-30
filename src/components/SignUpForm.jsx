516d16d99344f5820cc61e85099a2749a248b00e
Jordan Humphreys <mrsweaters@users.noreply.github.com>
2023-05-29 22:51:29 -0400

    Fix more className issues.

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -7,8 +7,8 @@ export function SignUpForm() {
 
   return (
     <PopupButton id="RamM3W1f" className="mt-8 group relative isolate flex-none rounded-md py-1.5 text-[0.8125rem]/6 font-semibold text-white pl-2.5 pr-[calc(9/16*1rem)]">
-      <span class="absolute inset-0 rounded-md bg-gradient-to-b from-white/80 to-white opacity-10 transition-opacity group-hover:opacity-15"></span>
-      <span class="absolute inset-0 rounded-md opacity-7.5 shadow-[inset_0_1px_1px_white] transition-opacity group-hover:opacity-10"></span>
+      <span className="absolute inset-0 rounded-md bg-gradient-to-b from-white/80 to-white opacity-10 transition-opacity group-hover:opacity-15"></span>
+      <span className="absolute inset-0 rounded-md opacity-7.5 shadow-[inset_0_1px_1px_white] transition-opacity group-hover:opacity-10"></span>
       Sign up for early access
     </PopupButton>
   )
